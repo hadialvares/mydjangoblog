cecadf9752655bd5789b7169a3df8c9656d5b86b
hadi <hadi.alvares@gmail.com>
2021-12-03 02:31:18 +0330

    create url for title

--- articles/views.py
+++ articles/views.py
@@ -6,7 +6,9 @@ def articles_list(request):
     articles = models.Article.objects.all().order_by('-date')
 
     args = {'articles':articles}
-    return render( request ,'articles/articleslist.html', args)
+    return render(request,'articles/articleslist.html', args)
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = models.Article.objects.get(slug = slug)
+    return render(request, 'articles/article_detail.html', {'article':article})
