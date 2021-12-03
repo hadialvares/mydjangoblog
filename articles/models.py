d8f9413d27ab98ba66aac36645908400957de7f2
hadi <hadi.alvares@gmail.com>
2021-12-03 11:16:29 +0330

    create image urls

--- articles/models.py
+++ articles/models.py
@@ -5,12 +5,12 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(default='default.jpg',blank=True)
 
-    # in add thumbnaile
     # in add author
 
     def __str__(self):        # توی پوشه ارتیکل برای عنوان مفاله تایتل را نشان میدهد
         return self.title
 
     def snippet(self):
-        return self.body[:50] + ' ...'     # بجای اینکه کل مقاله را نشان دهد فقط 50 کاراکتر نشان میدهد 
+        return self.body[:50] + ' ...'     # بجای اینکه کل مقاله را نشان دهد فقط 50 کاراکتر نشان میدهد
